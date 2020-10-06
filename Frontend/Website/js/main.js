c7eb2ac90151152d15364539d2553d476b78f1c2
carlosngv99 <carlosngv99@hotmail.com>
2020-10-05 18:03:32 -0600

    Codemirror implemented. Open file function finished. Pending fixing mode for editor.

--- Frontend/Website/js/main.js
+++ Frontend/Website/js/main.js
@@ -1,55 +1,118 @@
-"use strict";
+import CodeMirror from "../codemirror/src/codemirror.js";
 
-/*(function() {
-    let dest = location.hash.slice(1);
-    if (dest) {
-        let tab = document.getElementById(dest);
-        if(tab) {
-            tab.click();
-        }
-    }
-})();*/
-var tabsArray = Array.prototype.slice.apply(document.querySelectorAll(".tabs_item")); // converting into array
-var panelsArray = Array.prototype.slice.apply(document.querySelectorAll(".panels_item"));
+var tabsArray = Array.prototype.slice.apply(
+  document.querySelectorAll(".tabs_item")
+); // converting into array
+var panelsArray = Array.prototype.slice.apply(
+  document.querySelectorAll(".panels_item")
+);
 
-var fragment = document.createDocumentFragment();
-console.log(fragment);
-
-var open = document.getElementById("open").addEventListener("click", () => {
-    console.log("HOLA");
-});
-var cont = 1;
-var newTab = document.getElementById("new").addEventListener("click", () => {
-    cont = cont + 1
+// Open file
+const fileAux = document.getElementById("myFile");
+fileAux.addEventListener("change", (e) => {
+  tabsArray = Array.prototype.slice.apply(
+    document.querySelectorAll(".tabs_item")
+  ); // converting into array
+  panelsArray = Array.prototype.slice.apply(
+    document.querySelectorAll(".panels_item")
+  );
+  console.log(fileAux.files);
+  let file = fileAux.files[0];
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    cont = cont + 1;
     let tabs = document.getElementById("tabs"); // Tab container
-    let panels = document.getElementById("panels"); // Panel conatainer 
+    let panels = document.getElementById("panels"); // Panel conatainer
 
     // New tab
     let fragment = document.createDocumentFragment();
     let newTab = document.createElement("li");
-    newTab.setAttribute('class', 'tabs_item');
-    newTab.textContent = 'Tab ' + String(cont);
+    newTab.setAttribute("class", "tabs_item");
+    newTab.textContent = file.name;
     fragment.appendChild(newTab);
     tabs.appendChild(fragment);
 
     // New panel
     fragment = document.createDocumentFragment();
-    let newPanelContainer = document.createElement('div');
-    newPanelContainer.setAttribute('class', 'panels_item');
-    let panelContent = document.createElement("p");
-    panelContent.textContent= "HOLA";
+    let newPanelContainer = document.createElement("div");
+    newPanelContainer.setAttribute("class", "panels_item");
+    let panelContent = document.createElement("textarea");
+    panelContent.setAttribute("id", "editor");
+    panelContent.setAttribute("class", "editor");
+
+    var contents = e.target.result;
+    panelContent.value = contents;
+
     newPanelContainer.appendChild(panelContent);
     fragment.appendChild(newPanelContainer);
     panels.appendChild(fragment);
 
     // updates the tabs and panels array
-    tabsArray = Array.prototype.slice.apply(document.querySelectorAll(".tabs_item"));
-    panelsArray = Array.prototype.slice.apply(document.querySelectorAll(".panels_item"));
+    tabsArray = Array.prototype.slice.apply(
+      document.querySelectorAll(".tabs_item")
+    );
+    panelsArray = Array.prototype.slice.apply(
+      document.querySelectorAll(".panels_item")
+    );
+    console.log(tabsArray);
 
+    newEditor(cont);
+  };
+  console.log(file);
+  reader.readAsText(file);
 });
+
+var fragment = document.createDocumentFragment();
+console.log(fragment);
+
+var cont = 1;
+
+document.getElementById("new").addEventListener("click", () => {
+  cont = cont + 1;
+  let tabs = document.getElementById("tabs"); // Tab container
+  let panels = document.getElementById("panels"); // Panel conatainer
+
+  // New tab
+  let fragment = document.createDocumentFragment();
+  let newTab = document.createElement("li");
+  newTab.setAttribute("class", "tabs_item");
+  newTab.textContent = "Tab " + String(cont);
+  fragment.appendChild(newTab);
+  tabs.appendChild(fragment);
+
+  // New panel
+  fragment = document.createDocumentFragment();
+  let newPanelContainer = document.createElement("div");
+  newPanelContainer.setAttribute("class", "panels_item");
+  let panelContent = document.createElement("textarea");
+  panelContent.setAttribute("id", "editor");
+  panelContent.setAttribute("class", "editor");
+
+  newPanelContainer.appendChild(panelContent);
+  fragment.appendChild(newPanelContainer);
+  panels.appendChild(fragment);
+
+  // updates the tabs and panels array
+  tabsArray = Array.prototype.slice.apply(
+    document.querySelectorAll(".tabs_item")
+  );
+  panelsArray = Array.prototype.slice.apply(
+    document.querySelectorAll(".panels_item")
+  );
+
+  newEditor(cont);
+});
+
 (function (d) {
-  
   d.getElementById("tabs").addEventListener("click", (e) => {
+    tabsArray = Array.prototype.slice.apply(
+      document.querySelectorAll(".tabs_item")
+    ); // converting into array
+    panelsArray = Array.prototype.slice.apply(
+      document.querySelectorAll(".panels_item")
+    );
+
+    console.log(tabsArray);
     if (e.target.classList.contains("tabs_item")) {
       let i = tabsArray.indexOf(e.target);
       tabsArray.map((tab) => {
@@ -63,3 +126,19 @@ var newTab = document.getElementById("new").addEventListener("click", () => {
     }
   });
 })(document);
+
+function newEditor(index) {
+  index = index - 1;
+  let editors = Array.prototype.slice.apply(
+    document.querySelectorAll(".editor")
+  );
+  console.log(editors);
+  var editor = CodeMirror.fromTextArea(editors[index], {
+    lineNumbers: true,
+    mode: "text/x-java",
+    theme: 'dracula',
+    matchBrackets: true,
+    autoCloseBrackets: true,
+  });
+  editor.setSize("470", "450");
+}
--- Frontend/main.go
+++ Frontend/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
-
-	rice "github.com/GeertJohan/go.rice"
-	"github.com/gorilla/mux"
 )
 
+func index(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("index.html"))
+	t.Execute(w, "")
+}
+
 func main() {
-	router := mux.NewRouter()
-	router.PathPrefix("/").Handler(http.FileServer(rice.MustFindBox("website").HTTPBox()))
-	http.ListenAndServe(":3000", router)
+	//router := mux.NewRouter()
+	http.Handle("/Website/styles/", http.StripPrefix("/Website/styles/", http.FileServer(http.Dir("Website/styles/"))))
+	http.Handle("/Website/js/", http.StripPrefix("/Website/js/", http.FileServer(http.Dir("Website/js/"))))
+	http.Handle("/Website/codemirror/", http.StripPrefix("/Website/codemirror/", http.FileServer(http.Dir("Website/codemirror/"))))
+
+	http.HandleFunc("/", index)
+	//router.PathPrefix("/").Handler(http.FileServer(rice.MustFindBox("website").HTTPBox()))
+	http.ListenAndServe(":3000", nil)
 }
