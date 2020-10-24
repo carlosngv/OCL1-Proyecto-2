f0bf1ca9b6905b6e1b0b53cc9f93a149f3bc8efb
carlosngv99 <carlosngv99@hotmail.com>
2020-10-24 17:26:06 -0600

    Download python file finished.

---
+++ PythonServer/public/traduccion.py
@@ -0,0 +1,37 @@
+class interfaz:
+	self hola(inta);
+class Prueba:
+	def main():
+		while x!= "verdadero":
+			if a< 1 :
+				a = 1 
+
+
+		while m<= 0 :
+			for i in range(0,1):
+				hola(a)
+
+
+		if a< 0 :
+			print("HOLA")
+
+		elif r!= n:
+			a = 1 
+			m = 0 
+
+		else:
+			return "retornando :)"
+
+	if __name__ = “__main__”:
+ 		main()
+	def devolucion(entrada):
+		hola(a)
+
+
+class HashTable:
+	def hola(a):
+
+	def adios():
+		print("HOLA")
+
+
--- PythonServer/server/routes/scanRouter.js
+++ PythonServer/server/routes/scanRouter.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const generateHTML = require('../reports/tokenList');
 const generateHTMLErrors = require('../reports/errorList');
+const fs = require("fs");
 
 const Scanner = require("../scanner/scanner");
 const Parser = require("../parser/parser2");
@@ -13,6 +14,11 @@ scanRouter.post("/", (req, res) => {
   generateHTMLErrors(newScanner.errorList);
   var newParser = new Parser(newScanner.tokenList);
   newParser.parse()
+  fs.writeFile('public/traduccion.py', newParser.stringTraduccion, (error) => {
+    if(error){
+      console.log(error)
+    } 
+  });
   /* console.log(newParser.stringTraduccion);
   console.log('---------------------------------------------------------------------------------');
   console.log('ERRORES', newParser.errorList); */
@@ -22,6 +28,10 @@ scanRouter.post("/", (req, res) => {
   res.json({
     errors: newParser.errorList,
   });
+});
+
+scanRouter.get('/downloadTranslation', (req, res) => {
+  res.download('public/traduccion.py', 'traduccion.py')
 });
 
 module.exports = scanRouter;
